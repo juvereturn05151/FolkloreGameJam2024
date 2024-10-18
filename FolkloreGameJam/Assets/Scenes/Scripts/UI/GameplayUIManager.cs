20f045b9b17453ec6ab4a2d5a4c683675f3e406d
juvereturn@gmail.com <Rusty05151>
2024-10-17 19:21:44 -0700

    add restart functionality

--- FolkloreGameJam/Assets/Scenes/Scripts/UI/GameplayUIManager.cs
+++ FolkloreGameJam/Assets/Scenes/Scripts/UI/GameplayUIManager.cs
@@ -107,4 +107,9 @@ public class GameplayUIManager : MonoBehaviour
 
         GameManager.Instance.ApplyGameOver();
     }
+
+    public void Restart() 
+    {
+        GameManager.Instance.PlayAgain();
+    }
 }
