a7de9c307021d224eaadea9d3e1aeaebe4f1e841
juvereturn@gmail.com <Rusty05151>
2024-11-04 23:46:23 -0800

    make cutting head not killing human anymore

--- FolkloreGameJam/Assets/Scenes/Scripts/GameElement/Human/HumanBody.cs
+++ FolkloreGameJam/Assets/Scenes/Scripts/GameElement/Human/HumanBody.cs
@@ -5,9 +5,26 @@ public class HumanBody : MonoBehaviour
     [SerializeField]
     private HumanPart _head;
 
+    [SerializeField]
+    private HumanPart _neck;
+
+    [SerializeField]
+    private HumanPart _body;
+
+    [SerializeField]
+    private HumanPart _leg;
+
     private void Start()
     {
-        _head.OnPartDestroyed.AddListener(DestroyItself);
+        //_head.OnPartDestroyed.AddListener(DestroyItself);
+    }
+
+    private void Update()
+    {
+        if (_head == null && _neck == null && _body == null && _leg == null) 
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     private void DestroyItself()
