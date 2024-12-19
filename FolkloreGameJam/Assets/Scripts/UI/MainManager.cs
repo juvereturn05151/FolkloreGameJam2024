e4e9720d30a9d9f240576c78d12d489dbf950ed4
juvereturn@gmail.com <Rusty05151>
2024-12-18 22:41:43 -0800

    add credit scene and navigation

--- FolkloreGameJam/Assets/Scripts/UI/MainManager.cs
+++ FolkloreGameJam/Assets/Scripts/UI/MainManager.cs
@@ -7,6 +7,17 @@ public class MainManager : MonoBehaviour
     [SerializeField]
     private HumanPart humanPart;
 
+    [SerializeField]
+    private string tutorialSceneName = "TutorialScene";
+
+    [SerializeField]
+    private string creditsSceneName = "Credits";
+
+    [SerializeField]
+    private string menuSceneName = "MainMenu";
+
+    private string targetSceneName;
+
     private void Start()
     {
         if (SteamManager.Initialized)
@@ -17,29 +28,57 @@ public class MainManager : MonoBehaviour
 
         SteamLeaderboardManager.Init();
 
-        humanPart.OnPartDestroyed.AddListener(OnClickStart);
+        // Add listener for part destruction
+        if(humanPart != null)
+        humanPart.OnPartDestroyed.AddListener(() => OnPartDestroyed(tutorialSceneName));
     }
 
     public void OnClickStart()
     {
-        //Call start game
+        // Set target scene to the tutorial scene
+        targetSceneName = tutorialSceneName;
+
+        // Call start game
         FadingUI.Instance.StartFadeIn();
-        FadingUI.Instance.OnStopFading.AddListener(LoadTutorialScene);
-    }
-
-    private void LoadTutorialScene()
-    {
-        SceneManager.LoadScene("TutorialScene");
-    }
-
-    public void ClickToExit()
-    {
-        //Call start game
-        Application.Quit();
+        FadingUI.Instance.OnStopFading.AddListener(LoadTargetScene);
     }
 
     public void ClickToGoToCredit()
     {
+        // Set target scene to the credits scene
+        targetSceneName = creditsSceneName;
+
+        // Call start game
         FadingUI.Instance.StartFadeIn();
+        FadingUI.Instance.OnStopFading.AddListener(LoadTargetScene);
+    }
+
+    public void ClickToMainMenu()
+    {
+        // Set target scene to the main menu scene
+        targetSceneName = menuSceneName;
+
+        // Call start game
+        FadingUI.Instance.StartFadeIn();
+        FadingUI.Instance.OnStopFading.AddListener(LoadTargetScene);
+    }
+
+    private void OnPartDestroyed(string sceneName)
+    {
+        // Set target scene based on which event is triggered
+        targetSceneName = sceneName;
+
+        FadingUI.Instance.StartFadeIn();
+        FadingUI.Instance.OnStopFading.AddListener(LoadTargetScene);
+    }
+
+    private void LoadTargetScene()
+    {
+        SceneManager.LoadScene(targetSceneName);
+    }
+
+    public void ClickToExit()
+    {
+        Application.Quit();
     }
 }
