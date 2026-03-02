36c0093a9a0438a038f84329ea7482226f09ab46
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2026-03-01 19:25:01 -0800

    update main menu

--- FolkloreGameJam/Assets/SliceSufferServe_Project/Scripts/GameElement/SlicePart/HumanPart.cs
+++ FolkloreGameJam/Assets/SliceSufferServe_Project/Scripts/GameElement/SlicePart/HumanPart.cs
@@ -54,6 +54,8 @@ public class HumanPart : MonoBehaviour
 
         Sliced?.Invoke(transform.position, feedbackRequests);
 
+        OnPartDestroyed?.Invoke();
+
         Destroy(gameObject);
     }
 }
