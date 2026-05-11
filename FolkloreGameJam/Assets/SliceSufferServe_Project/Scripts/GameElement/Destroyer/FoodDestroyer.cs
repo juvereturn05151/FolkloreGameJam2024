7c9dbdeb8285e6cb956d5a5eaa1492df84c6c644
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2026-05-11 02:28:09 -0700

    update object tag

---
+++ FolkloreGameJam/Assets/SliceSufferServe_Project/Scripts/GameElement/Destroyer/FoodDestroyer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class FoodDestroyer : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Food"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ FolkloreGameJam/Assets/SliceSufferServe_Project/Scripts/GameElement/Destroyer/HumanDestroyer.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class HumanDestroyer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<HumanPart>() is HumanPart part)
+        {
+            Destroy(part.gameObject);
+            if (part.OnPartDestroyed != null)
+            {
+                part.OnPartDestroyed.Invoke();
+            }
+            return;
+        }
+    }
+}
