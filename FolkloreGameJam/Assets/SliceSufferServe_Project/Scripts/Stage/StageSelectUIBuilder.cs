1718f99214a130f24bd3fa196c62fddbcd3f46eb
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2026-05-14 22:42:49 -0700

    add stage select

--- FolkloreGameJam/Assets/SliceSufferServe_Project/Scripts/Stage/StageSelectUIBuilder.cs
+++ FolkloreGameJam/Assets/SliceSufferServe_Project/Scripts/Stage/StageSelectUIBuilder.cs
@@ -11,6 +11,8 @@ public class StageSelectUIBuilder : MonoBehaviour
     [SerializeField] private string backSceneName = "GameModeSelect";
     [SerializeField] private Vector2 nodeSize = new Vector2(320, 300);
     [SerializeField] private float nodeSpacing = 360f;
+    [SerializeField] private float rowSpacing = 320f;
+    [SerializeField] private int maxNodesPerRow = 4;
     [SerializeField] private string firstTutorialName = "First Tutorial";
     [SerializeField] private string secondTutorialName = "Second Tutorial";
 
@@ -64,23 +66,22 @@ public class StageSelectUIBuilder : MonoBehaviour
         int levelCount = levelDatabase == null ? 0 : levelDatabase.Count;
         int tutorialCount = GetTutorialNodeCount(levelCount);
         int nodeCount = levelCount + tutorialCount;
-        float startX = -nodeSpacing * (nodeCount - 1) * 0.5f;
         int nodeIndex = 0;
 
         if (levelCount > 0)
         {
-            CreateTutorialNode(nodeIndex++, 0, firstTutorialName, startX);
+            CreateTutorialNode(nodeIndex++, 0, firstTutorialName, nodeCount);
         }
 
         for (int i = 0; i < levelCount; i++)
         {
             if (i == 3)
             {
-                CreateTutorialNode(nodeIndex++, 3, secondTutorialName, startX);
+                CreateTutorialNode(nodeIndex++, 3, secondTutorialName, nodeCount);
             }
 
             StageLevelConfig levelConfig = levelDatabase.GetLevel(i);
-            StageSelectButton stageNode = CreateNode(nodeIndex++, startX);
+            StageSelectButton stageNode = CreateNode(nodeIndex++, nodeCount);
             stageNode.Configure(stageSelectManager, i, levelConfig);
         }
     }
@@ -91,13 +92,13 @@ public class StageSelectUIBuilder : MonoBehaviour
         return levelCount > 3 ? tutorialCount + 1 : tutorialCount;
     }
 
-    private void CreateTutorialNode(int nodeIndex, int targetLevelIndex, string tutorialName, float startX)
+    private void CreateTutorialNode(int nodeIndex, int targetLevelIndex, string tutorialName, int nodeCount)
     {
-        StageSelectButton stageNode = CreateNode(nodeIndex, startX);
+        StageSelectButton stageNode = CreateNode(nodeIndex, nodeCount);
         stageNode.ConfigureTutorial(stageSelectManager, targetLevelIndex, tutorialName);
     }
 
-    private StageSelectButton CreateNode(int nodeIndex, float startX)
+    private StageSelectButton CreateNode(int nodeIndex, int nodeCount)
     {
         StageSelectButton stageNode = Instantiate(stageNodePrefab, levelListRoot);
 
@@ -106,11 +107,24 @@ public class StageSelectUIBuilder : MonoBehaviour
         {
             stageNodeRect.anchorMin = new Vector2(0.5f, 0.5f);
             stageNodeRect.anchorMax = new Vector2(0.5f, 0.5f);
-            stageNodeRect.anchoredPosition = new Vector2(startX + nodeSpacing * nodeIndex, 0);
+            stageNodeRect.anchoredPosition = GetNodePosition(nodeIndex, nodeCount);
             stageNodeRect.sizeDelta = nodeSize;
         }
 
         return stageNode;
+    }
+
+    private Vector2 GetNodePosition(int nodeIndex, int nodeCount)
+    {
+        int nodesPerRow = Mathf.Max(1, maxNodesPerRow);
+        int rowCount = Mathf.CeilToInt(nodeCount / (float)nodesPerRow);
+        int rowIndex = nodeIndex / nodesPerRow;
+        int columnIndex = nodeIndex % nodesPerRow;
+        int rowNodeCount = Mathf.Min(nodesPerRow, nodeCount - rowIndex * nodesPerRow);
+
+        float startX = -nodeSpacing * (rowNodeCount - 1) * 0.5f;
+        float startY = rowSpacing * (rowCount - 1) * 0.5f;
+        return new Vector2(startX + nodeSpacing * columnIndex, startY - rowSpacing * rowIndex);
     }
 
     private void ClearLevelList()
