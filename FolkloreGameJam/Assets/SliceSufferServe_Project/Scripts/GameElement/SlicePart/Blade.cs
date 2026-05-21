5900e910d8cb0abef7b8ed69b254ae978987a210
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2026-05-21 13:40:37 -0700

    refactor asset names

--- FolkloreGameJam/Assets/SliceSufferServe_Project/Scripts/GameElement/SlicePart/Blade.cs
+++ FolkloreGameJam/Assets/SliceSufferServe_Project/Scripts/GameElement/SlicePart/Blade.cs
@@ -2,33 +2,37 @@ using UnityEngine;
 
 public class Blade : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject bladeTrailPrefab;
+    [SerializeField]
+    private float minCuttingVelocity = .001f;
+    [SerializeField]
+    private Rigidbody2D rb;
+    [SerializeField]
+    private CircleCollider2D circleCollider;
 
-    public GameObject bladeTrailPrefab;
-    public float minCuttingVelocity = .001f;
+    private Vector2 previousPosition;
+    private GameObject currentBladeTrail;
+    private Camera cam;
+    private bool isCutting = false;
 
-    bool isCutting = false;
-
-    Vector2 previousPosition;
-
-    GameObject currentBladeTrail;
-
-    Rigidbody2D rb;
-    Camera cam;
-    CircleCollider2D circleCollider;
-    public CircleCollider2D CircleCollider => circleCollider;
-
-    void Start()
+    private void Start()
     {
         cam = Camera.main;
-        rb = GetComponent<Rigidbody2D>();
-        circleCollider = GetComponent<CircleCollider2D>();
+
+        if (rb == null) 
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        if (circleCollider == null)
+        {
+            circleCollider = GetComponent<CircleCollider2D>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
-
         if (Input.GetMouseButtonDown(0))
         {
             StartCutting();
@@ -42,10 +46,9 @@ public class Blade : MonoBehaviour
         {
             UpdateCut();
         }
-
     }
 
-    void UpdateCut()
+    private void UpdateCut()
     {
         Vector2 newPosition = cam.ScreenToWorldPoint(Input.mousePosition);
         rb.position = newPosition;
@@ -64,7 +67,7 @@ public class Blade : MonoBehaviour
         previousPosition = newPosition;
     }
 
-    void StartCutting()
+    private void StartCutting()
     {
         isCutting = true;
         currentBladeTrail = Instantiate(bladeTrailPrefab, transform);
@@ -73,7 +76,7 @@ public class Blade : MonoBehaviour
         SoundManager.instance.PlaySFX("SFX_Slice");
     }
 
-    void StopCutting()
+    private void StopCutting()
     {
         isCutting = false;
         currentBladeTrail.transform.SetParent(null);
