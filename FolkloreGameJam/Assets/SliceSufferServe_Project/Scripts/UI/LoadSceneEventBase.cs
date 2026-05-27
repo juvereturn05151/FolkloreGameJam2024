d03136e08f1325a06845a639dfd1fe89bb496b16
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2026-05-27 03:45:13 -0700

    add classic mode

--- FolkloreGameJam/Assets/SliceSufferServe_Project/Scripts/UI/LoadSceneEventBase.cs
+++ FolkloreGameJam/Assets/SliceSufferServe_Project/Scripts/UI/LoadSceneEventBase.cs
@@ -7,9 +7,16 @@ public class LoadSceneEventBase : MonoBehaviour
 
     [SerializeField]
     private string targetSceneName;
+    [SerializeField]
+    private StageLevelConfig selectedLevelConfig;
 
     protected void OnLoadSceneEvent()
     {
+        if (selectedLevelConfig != null)
+        {
+            StageSelection.SelectLevel(selectedLevelConfig);
+        }
+
         FadingUI.Instance.StartFadeIn();
         FadingUI.Instance.OnStopFading.AddListener(LoadTargetScene);
     }
