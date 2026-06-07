7482db4e684ebd6db559ee021658012bcfdbd517
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2026-06-06 20:20:58 -0700

    Add slicing to go to next stage

--- FolkloreGameJam/Assets/SliceSufferServe_Project/Scripts/Stage/StageSelectButton.cs
+++ FolkloreGameJam/Assets/SliceSufferServe_Project/Scripts/Stage/StageSelectButton.cs
@@ -1,7 +1,9 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class StageSelectButton : MonoBehaviour
+public class StageSelectButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
 {
     private enum SelectMode
     {
@@ -18,9 +20,18 @@ public class StageSelectButton : MonoBehaviour
     [SerializeField] private Text titleText;
     [SerializeField] private Text starsText;
     [SerializeField] private Sprite lockSprite;
+    [SerializeField] private float cutSelectDistance = 80f;
+    [SerializeField] private float cutEffectDuration = 0.25f;
+    [SerializeField] private float cutEffectSeparation = 80f;
+    [SerializeField] private float cutEffectRotation = 9f;
 
     private SelectMode selectMode;
     private bool isUnlocked;
+    private bool isTrackingCut;
+    private bool cutSelectionTriggered;
+    private Vector2 cutStartScreenPosition;
+    private Vector2 lastScreenPosition;
+    private bool isPlayingCutEffect;
 
     private void Awake()
     {
@@ -43,19 +54,57 @@ public class StageSelectButton : MonoBehaviour
         }
     }
 
-    private void OnEnable()
-    {
-        if (button != null)
-        {
-            button.onClick.AddListener(SelectLevel);
-        }
-    }
-
     private void OnDisable()
     {
-        if (button != null)
+        StopTrackingCut();
+    }
+
+    private void Update()
+    {
+        if (!isTrackingCut)
         {
-            button.onClick.RemoveListener(SelectLevel);
+            return;
+        }
+
+        if (!Input.GetMouseButton(0))
+        {
+            StopTrackingCut();
+            return;
+        }
+
+        UpdateCutSelection(Input.mousePosition);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (eventData == null || eventData.button != PointerEventData.InputButton.Left)
+        {
+            return;
+        }
+
+        BeginTrackingCut(eventData.position);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        StopTrackingCut();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData == null || !Input.GetMouseButton(0))
+        {
+            return;
+        }
+
+        BeginTrackingCut(eventData.position);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData != null)
+        {
+            UpdateCutSelection(eventData.position);
         }
     }
 
@@ -231,6 +280,176 @@ public class StageSelectButton : MonoBehaviour
         }
 
         stageSelectManager.SelectLevel(levelIndex);
+    }
+
+    private void BeginTrackingCut(Vector2 screenPosition)
+    {
+        if (!isUnlocked || stageSelectManager == null)
+        {
+            return;
+        }
+
+        isTrackingCut = true;
+        cutSelectionTriggered = false;
+        cutStartScreenPosition = screenPosition;
+        lastScreenPosition = screenPosition;
+    }
+
+    private void UpdateCutSelection(Vector2 screenPosition)
+    {
+        if (!isTrackingCut || cutSelectionTriggered)
+        {
+            return;
+        }
+
+        float distanceFromStart = Vector2.Distance(cutStartScreenPosition, screenPosition);
+        float distanceFromLastPosition = Vector2.Distance(lastScreenPosition, screenPosition);
+        lastScreenPosition = screenPosition;
+
+        if (distanceFromStart < cutSelectDistance && distanceFromLastPosition < cutSelectDistance)
+        {
+            return;
+        }
+
+        cutSelectionTriggered = true;
+        StopTrackingCut();
+        StartCoroutine(PlayCutEffectThenSelect());
+    }
+
+    private void StopTrackingCut()
+    {
+        isTrackingCut = false;
+        cutSelectionTriggered = false;
+    }
+
+    private IEnumerator PlayCutEffectThenSelect()
+    {
+        if (isPlayingCutEffect)
+        {
+            yield break;
+        }
+
+        isPlayingCutEffect = true;
+
+        RectTransform sourceRect = transform as RectTransform;
+        if (sourceRect == null || sourceRect.rect.width <= 0f || sourceRect.rect.height <= 0f)
+        {
+            SelectLevel();
+            yield break;
+        }
+
+        Transform originalParent = transform.parent;
+        int originalSiblingIndex = transform.GetSiblingIndex();
+        CanvasGroup originalGroup = GetOrAddComponent<CanvasGroup>(gameObject);
+        originalGroup.alpha = 0f;
+        if (button != null)
+        {
+            button.interactable = false;
+        }
+
+        RectTransform leftPiece = CreateCutPiece("LeftCutPiece", sourceRect, originalParent, originalSiblingIndex, true);
+        RectTransform rightPiece = CreateCutPiece("RightCutPiece", sourceRect, originalParent, originalSiblingIndex + 1, false);
+        CanvasGroup leftGroup = leftPiece.GetComponent<CanvasGroup>();
+        CanvasGroup rightGroup = rightPiece.GetComponent<CanvasGroup>();
+
+        Vector2 leftStart = leftPiece.anchoredPosition;
+        Vector2 rightStart = rightPiece.anchoredPosition;
+        float elapsed = 0f;
+        float duration = Mathf.Max(0.01f, cutEffectDuration);
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.unscaledDeltaTime;
+            float t = Mathf.Clamp01(elapsed / duration);
+            float eased = 1f - Mathf.Pow(1f - t, 2f);
+
+            leftPiece.anchoredPosition = leftStart + new Vector2(-cutEffectSeparation, -cutEffectSeparation * 0.35f) * eased;
+            rightPiece.anchoredPosition = rightStart + new Vector2(cutEffectSeparation, cutEffectSeparation * 0.35f) * eased;
+            leftPiece.localRotation = Quaternion.Euler(0f, 0f, cutEffectRotation * eased);
+            rightPiece.localRotation = Quaternion.Euler(0f, 0f, -cutEffectRotation * eased);
+
+            float alpha = 1f - t;
+            leftGroup.alpha = alpha;
+            rightGroup.alpha = alpha;
+            yield return null;
+        }
+
+        Destroy(leftPiece.gameObject);
+        Destroy(rightPiece.gameObject);
+        SelectLevel();
+    }
+
+    private RectTransform CreateCutPiece(string pieceName, RectTransform sourceRect, Transform parent, int siblingIndex, bool leftSide)
+    {
+        GameObject maskObject = new GameObject(pieceName, typeof(RectTransform), typeof(RectMask2D), typeof(CanvasGroup));
+        maskObject.transform.SetParent(parent, false);
+        maskObject.transform.SetSiblingIndex(siblingIndex);
+
+        RectTransform maskRect = maskObject.GetComponent<RectTransform>();
+        maskRect.anchorMin = sourceRect.anchorMin;
+        maskRect.anchorMax = sourceRect.anchorMax;
+        maskRect.pivot = sourceRect.pivot;
+        maskRect.anchoredPosition = sourceRect.anchoredPosition + new Vector2(leftSide ? -sourceRect.rect.width * 0.25f : sourceRect.rect.width * 0.25f, 0f);
+        maskRect.sizeDelta = new Vector2(sourceRect.rect.width * 0.5f, sourceRect.rect.height);
+        maskRect.localScale = sourceRect.localScale;
+
+        GameObject clone = Instantiate(gameObject, maskObject.transform);
+        clone.name = leftSide ? "LeftVisual" : "RightVisual";
+        StripInteractiveComponents(clone);
+        ResetCanvasGroups(clone);
+
+        RectTransform cloneRect = clone.transform as RectTransform;
+        cloneRect.anchorMin = new Vector2(0.5f, 0.5f);
+        cloneRect.anchorMax = new Vector2(0.5f, 0.5f);
+        cloneRect.pivot = sourceRect.pivot;
+        cloneRect.anchoredPosition = new Vector2(leftSide ? sourceRect.rect.width * 0.25f : -sourceRect.rect.width * 0.25f, 0f);
+        cloneRect.sizeDelta = sourceRect.sizeDelta;
+        cloneRect.localScale = Vector3.one;
+        cloneRect.localRotation = Quaternion.identity;
+
+        return maskRect;
+    }
+
+    private void StripInteractiveComponents(GameObject clone)
+    {
+        StageSelectButton[] stageButtons = clone.GetComponentsInChildren<StageSelectButton>(true);
+        for (int i = 0; i < stageButtons.Length; i++)
+        {
+            Destroy(stageButtons[i]);
+        }
+
+        Selectable[] selectables = clone.GetComponentsInChildren<Selectable>(true);
+        for (int i = 0; i < selectables.Length; i++)
+        {
+            Destroy(selectables[i]);
+        }
+
+        UIClickSoundPlayer[] clickSounds = clone.GetComponentsInChildren<UIClickSoundPlayer>(true);
+        for (int i = 0; i < clickSounds.Length; i++)
+        {
+            Destroy(clickSounds[i]);
+        }
+    }
+
+    private void ResetCanvasGroups(GameObject clone)
+    {
+        CanvasGroup[] canvasGroups = clone.GetComponentsInChildren<CanvasGroup>(true);
+        for (int i = 0; i < canvasGroups.Length; i++)
+        {
+            canvasGroups[i].alpha = 1f;
+            canvasGroups[i].interactable = false;
+            canvasGroups[i].blocksRaycasts = false;
+        }
+    }
+
+    private static T GetOrAddComponent<T>(GameObject target) where T : Component
+    {
+        if (target.TryGetComponent(out T component))
+        {
+            return component;
+        }
+
+        return target.AddComponent<T>();
     }
 
     private static int GetSavedStars(string levelId)
